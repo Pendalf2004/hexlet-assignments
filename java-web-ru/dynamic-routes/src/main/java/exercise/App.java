c87c3b86781efd82f0940ff3ec02c1e13cc38c07
@hexlet/cli <support@hexlet.io>
2024-09-02 09:45:13 +0300

    submit java-web-ru/dynamic-routes

--- java-web-ru/dynamic-routes/src/main/java/exercise/App.java
+++ java-web-ru/dynamic-routes/src/main/java/exercise/App.java
@@ -23,12 +23,16 @@ public final class App {
 
         // BEGIN
         app.get("/companies/{id}", ctx -> {
-           var id = ctx.pathParamAsClass("id", Integer.class).get();
-           try {
-               var company = COMPANIES.get(id);
-               ctx.json(company);
-           } catch (NoValueForKeyException ex) {
-               ctx.json("Company not found");
+           var id = ctx.pathParamAsClass("id", String.class).get();
+           var notFound = true;
+           for (var company : COMPANIES) {
+               if (company.get(id).equals(id)) {
+                   notFound = false;
+                   ctx.json(company);
+               }
+           }
+           if (notFound) {
+               throw new NotFoundResponse("User not found");
            }
 
         });
